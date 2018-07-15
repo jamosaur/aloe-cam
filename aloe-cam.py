e775da87ffa46490b56156ad53ee4e610f303083
jamosaur <j.wallen.jones@googlemail.com>
2018-07-15 14:05:12 +0100

    add some prints

--- aloe-cam.py
+++ aloe-cam.py
@@ -5,6 +5,6 @@ import photo
 
 while True:
     if photo.take_photo():
-        #tweet.tweet_image('photo.jpg')
+        tweet.tweet_image('photo.jpg')
 
     time.sleep(float(config.SLEEP_TIME))
--- filesystem.py
+++ filesystem.py
@@ -9,11 +9,11 @@ def get_latest_file_number():
         return 1
     latest_file = max(list_of_files, key=os.path.getctime)
     _, filename = os.path.split(latest_file)
-    print("Latest Filename " + filename)
-    return int(filename.split(".")[0])
+    return int(filename.split(".")[0]) + 1
 
 
 def copy_photo_to_local_storage():
     file_name = "%010d" % get_latest_file_number() + ".jpg"
+    print("Saving file: " + file_name)
     os.system("cp -f photo.jpg " + config.LOCAL_STORAGE_LOCATION)
     os.system("mv " + config.LOCAL_STORAGE_LOCATION + "photo.jpg " + config.LOCAL_STORAGE_LOCATION + file_name)
