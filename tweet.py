110f0ad48570ccf64ea50430e7af130be83851b8
jamosaur <j.wallen.jones@googlemail.com>
2018-07-22 22:39:46 +0100

    bad exception handling again

--- tweet.py
+++ tweet.py
@@ -6,10 +6,14 @@ import datetime
 def tweet_image(image_path=None):
     if config.TWITTER_ENABLED:
         if image_path:
-            auth = tweepy.OAuthHandler(config.CONSUMER_TOKEN, config.CONSUMER_SECRET)
-            auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_SECRET)
-            api = tweepy.API(auth)
-            current_time = datetime.datetime.now()
-            status = current_time.strftime("%c") + " #aloe #aloevera #timelapse #grow #growcam"
-            api.update_with_media(image_path, status)
-            print("Tweeted!")
+            try:
+                auth = tweepy.OAuthHandler(config.CONSUMER_TOKEN, config.CONSUMER_SECRET)
+                auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_SECRET)
+                api = tweepy.API(auth)
+                current_time = datetime.datetime.now()
+                status = current_time.strftime("%c") + " #aloe #aloevera #timelapse #grow #growcam"
+                api.update_with_media(image_path, status)
+                print("Tweeted!")
+            except:
+                print('Failed to tweet...')
+                pass
