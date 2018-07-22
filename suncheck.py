6d5c20619156fe838522635f2195c3bee366be9d
jamosaur <j.wallen.jones@googlemail.com>
2018-07-22 22:38:32 +0100

    bad exception handling

--- suncheck.py
+++ suncheck.py
@@ -6,46 +6,50 @@ import time
 
 
 def check_sun():
-    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-    now = datetime.datetime.now()
-    now_unix = int(time.mktime(datetime.datetime.now().timetuple()))
+    try:
 
-    url = "https://api.sunrise-sunset.org/json?lat=" + config.LAT + \
-          "&lng=" + config.LNG + "&formatted=0"
-    response = urllib.urlopen(url)
-    today = json.loads(response.read())
+        tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+        now = datetime.datetime.now()
+        now_unix = int(time.mktime(datetime.datetime.now().timetuple()))
 
-    url = "https://api.sunrise-sunset.org/json?lat=" + config.LAT + \
-          "&lng=" + config.LNG + "&formatted=0&date=" + str(tomorrow)
-    response = urllib.urlopen(url)
-    tomorrow = json.loads(response.read())
+        url = "https://api.sunrise-sunset.org/json?lat=" + config.LAT + \
+              "&lng=" + config.LNG + "&formatted=0"
+        response = urllib.urlopen(url)
+        today = json.loads(response.read())
 
-    today_rise = datetime.datetime.strptime(today["results"]["civil_twilight_begin"], "%Y-%m-%dT%H:%M:%S+00:00")
-    today_set = datetime.datetime.strptime(today["results"]["civil_twilight_end"], "%Y-%m-%dT%H:%M:%S+00:00")
-    tomorrow_rise = datetime.datetime.strptime(tomorrow["results"]["civil_twilight_begin"], "%Y-%m-%dT%H:%M:%S+00:00")
-    tomorrow_set = datetime.datetime.strptime(tomorrow["results"]["civil_twilight_end"], "%Y-%m-%dT%H:%M:%S+00:00")
+        url = "https://api.sunrise-sunset.org/json?lat=" + config.LAT + \
+              "&lng=" + config.LNG + "&formatted=0&date=" + str(tomorrow)
+        response = urllib.urlopen(url)
+        tomorrow = json.loads(response.read())
 
-    sleep = None
-    wake_time = None
+        today_rise = datetime.datetime.strptime(today["results"]["civil_twilight_begin"], "%Y-%m-%dT%H:%M:%S+00:00")
+        today_set = datetime.datetime.strptime(today["results"]["civil_twilight_end"], "%Y-%m-%dT%H:%M:%S+00:00")
+        tomorrow_rise = datetime.datetime.strptime(tomorrow["results"]["civil_twilight_begin"], "%Y-%m-%dT%H:%M:%S+00:00")
+        tomorrow_set = datetime.datetime.strptime(tomorrow["results"]["civil_twilight_end"], "%Y-%m-%dT%H:%M:%S+00:00")
 
-    if now > today_set:
-        # Sleep until tomorrows sunrise
-        wake_time = tomorrow["results"]["civil_twilight_begin"]
-        wake = datetime.datetime.strptime(tomorrow["results"]["civil_twilight_begin"], "%Y-%m-%dT%H:%M:%S+00:00")
-        wake_unix = int(time.mktime(wake.timetuple()))
-        sleep = wake_unix - now_unix
-    if now < today_rise:
-        # Sleep until todays sunrise
-        wake_time = today["results"]["civil_twilight_begin"]
-        wake = datetime.datetime.strptime(today["results"]["civil_twilight_begin"], "%Y-%m-%dT%H:%M:%S+00:00")
-        wake_unix = int(time.mktime(wake.timetuple()))
-        sleep = wake_unix - now_unix
+        sleep = None
+        wake_time = None
 
-    if sleep:
-        print("Sleeping for " + str(sleep) + " + 1 seconds, until " + str(wake_time))
-        sleep = sleep + 1
-        time.sleep(sleep)
-        check_sun()
+        if now > today_set:
+            # Sleep until tomorrows sunrise
+            wake_time = tomorrow["results"]["civil_twilight_begin"]
+            wake = datetime.datetime.strptime(tomorrow["results"]["civil_twilight_begin"], "%Y-%m-%dT%H:%M:%S+00:00")
+            wake_unix = int(time.mktime(wake.timetuple()))
+            sleep = wake_unix - now_unix
+        if now < today_rise:
+            # Sleep until todays sunrise
+            wake_time = today["results"]["civil_twilight_begin"]
+            wake = datetime.datetime.strptime(today["results"]["civil_twilight_begin"], "%Y-%m-%dT%H:%M:%S+00:00")
+            wake_unix = int(time.mktime(wake.timetuple()))
+            sleep = wake_unix - now_unix
+
+        if sleep:
+            print("Sleeping for " + str(sleep) + " + 1 seconds, until " + str(wake_time))
+            sleep = sleep + 1
+            time.sleep(sleep)
+            check_sun()
+    except:
+        pass
 
 
 if __name__ == '__main__':
